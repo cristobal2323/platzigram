7880d691e320ae72f3daecc23dddc0c96d0e227e
Cristóbal Maturana <cri.maturana@gmail.com>
2017-04-15 00:07:27 -0300

    comienzo proyecto

---
+++ server.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var multer  = require('multer');
+var ext = require('file-extension');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, +Date.now() + '.' + ext(file.originalname))
+  }
+})
+ 
+var upload = multer({ storage: storage }).single('picture');
+
+var app = express();
+
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Platzigram' });
+})
+
+app.get('/signup', function (req, res) {
+  res.render('index', { title: 'Platzigram - Signup' });
+})
+
+app.get('/signin', function (req, res) {
+  res.render('index', { title: 'Platzigram - Signin' });
+})
+
+app.get('/api/pictures', function (req, res, next) {
+  var pictures = [
+    {
+      user: {
+        username: 'slifszyc',
+        avatar: 'https://scontent.fgru3-1.fna.fbcdn.net/v/t1.0-9/16508225_10211891840672106_7303338519488516079_n.jpg?oh=cd7f4a1a322a85eded353d7be4515e98&oe=5927B60A'
+      },
+      url: 'office.jpg',
+      likes: 0,
+      liked: false,
+      createdAt: new Date().getTime()
+    },
+    {
+      user: {
+        username: 'slifszyc',
+        avatar: 'https://scontent.fgru3-1.fna.fbcdn.net/v/t1.0-9/16508225_10211891840672106_7303338519488516079_n.jpg?oh=cd7f4a1a322a85eded353d7be4515e98&oe=5927B60A'
+      },
+      url: 'office.jpg',
+      likes: 1,
+      liked: true,
+      createdAt: new Date().setDate(new Date().getDate() - 10)
+    }
+  ];
+
+  setTimeout(function () {
+    res.send(pictures);  
+  }, 2000)
+});
+
+app.post('/api/pictures', function (req, res) {
+  upload(req, res, function (err) {
+    if (err) {
+      return res.send(500, "Error uploading file");
+    }
+    res.send('File uploaded');
+  })
+})
+
+app.listen(3000, function (err) {
+  if (err) return console.log('Hubo un error'), process.exit(1);
+
+  console.log('Platzigram escuchando en el puerto 3000');
+})
